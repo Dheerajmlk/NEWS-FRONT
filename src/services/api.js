b1aeadac21e16293bf8f927591c48cdf096e4d85
dheerajmlk <dheerajmlk123@gmail.com>
2026-03-23 13:50:46 +0530

    final api fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL + "?v=2";
-
-console.log("API URL:", BASE_URL);
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
